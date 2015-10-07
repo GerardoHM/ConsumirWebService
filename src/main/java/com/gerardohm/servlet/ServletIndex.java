8f28ff194f5affc1992e1b07af03ec237598ca61
gerardohm <gerardohernandezmoreno180@gmail.com>
2015-10-06 19:27:21 -0500

    Se agregaron Backbone.js, jquery-1.11.3.js, underscore.js y se modifico el ServletIndex.java

--- src/main/java/com/gerardohm/servlet/ServletIndex.java
+++ src/main/java/com/gerardohm/servlet/ServletIndex.java
@@ -25,7 +25,7 @@ public class ServletIndex extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.getRequestDispatcher("index.jsp").forward(request, response);
+		request.getRequestDispatcher("index.html").forward(request, response);
 	}
 
 	/**
